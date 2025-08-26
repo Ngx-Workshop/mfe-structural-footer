920ef1f9c07539c896662cbed2d6f380971713d2
Wesley DuSell <dusellfam@gmail.com>
2025-08-26 00:27:24 -0400

    Add disable logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+
+import type { StructuralOverrideMode } from '@tmdjr/ngx-mfe-orchestrator-contracts';
 
 @Component({
   selector: 'ngx-footer-mfe',
   imports: [MatButtonModule],
   template: `
+    @if(mode() != 'disabled') {
     <footer class="footer">
-      Hello from <strong>ngx-footer-mfe</strong>! This is a microfrontend
       <a
         class="docs-footer-greeting"
         href="https://github.com/Ba5ik7/ngx-workshop"
@@ -23,13 +25,10 @@ import { MatButtonModule } from '@angular/material/button';
             d="M10 0C4.477 0 0 4.477 0 10c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V19c0 .27.16.59.67.5C17.14 18.16 20 14.42 20 10A10 10 0 0 0 10 0z"
           />
         </svg>
-        <!-- <img
-        class="workshop-logo docs-footer-github-logo"
-        src="/assets/img/github-circle-white-transparent.svg"
-        alt="github"> -->
         Made with ❤️
       </a>
     </footer>
+    }
   `,
   styles: [
     `
@@ -54,6 +53,8 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class App {
   protected title = 'ngx-footer-mfe';
+
+  mode = input<StructuralOverrideMode>('full');
 }
 
 // 👇 **IMPORTANT FOR DYMANIC LOADING**
